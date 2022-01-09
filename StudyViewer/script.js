e9fb653e08e0299cd7131150b41215629a1c9997
ROYCE MORIYASU <92335185+RoyceMoriyasu@users.noreply.github.com>
2022-01-09 18:08:55 +0000

    study viewer works now

--- StudyViewer/script.js
+++ StudyViewer/script.js
@@ -1,5 +1,22 @@
+var card = document.querySelector('.card');
+var cardNumber = 1;
+const question = document.getElementById('front');
+const answer = document.getElementById('back');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+setCard(contentArray[cardNumber - 1]);
+
+function setCard(text) {
+  question.innerHTML = text.my_question;
+  answer.innerHTML = text.my_answer;
+}
+
 function submitted() {
   readJson();
+  setCard(contentArray[cardNumber - 1]);
 }
 
 function readJson() {
@@ -12,4 +29,14 @@ function readJson() {
       localStorage.setItem('items', textFromFileLoaded);
     };
     fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
+function nextCard() {
+  cardNumber += 1;
+  setCard(contentArray[cardNumber - 1]);
+}
+
+function previousCard() {
+  cardNumber -= 1;
+  setCard(contentArray[cardNumber - 1]);
 }
